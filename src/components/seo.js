c142656215d4f3e2c7a28abe776f652013f5d7e7
Kevin Li <jshiansongli@gmail.com>
2022-06-29 16:14:31 -0400

    dagf

--- src/components/seo.js
+++ src/components/seo.js
@@ -56,10 +56,6 @@ const Seo = ({ description, lang, meta, title }) => {
           content: `website`,
         },
         {
-          property: `og:image`,
-          content: site.siteMetadata.image,
-        },
-        {
           name: `twitter:card`,
           content: `summary`,
         },
